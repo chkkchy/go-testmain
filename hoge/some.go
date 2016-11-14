3e12a9721f777639978ad66c73053ce6aed87098
Tamura Yukichi <tamura_yukichi@cyberagent.co.jp>
2016-11-14 18:36:57 +0900

    Modify

---
+++ hoge/some.go
@@ -0,0 +1,11 @@
+package hoge
+
+import "fmt"
+
+func Hogefunc1() {
+	fmt.Println("do hogefunc1")
+}
+
+func Hogefunc2() {
+	fmt.Println("do hogefunc2")
+}
--- hoge/some_test.go
+++ hoge/some_test.go
@@ -2,23 +2,24 @@ package hoge
 
 import (
 	"log"
+	"os"
 	"testing"
 )
 
 func TestMain(m *testing.M) {
-	panic()
-
 	log.Println("This gets run BEFORE any tests get run!")
-	//exitVal := m.Run()
+	exitVal := m.Run()
 	log.Println("This gets run AFTER any tests get run!")
-
-	//os.Exit(exitVal)
+	os.Exit(exitVal)
 }
 
 func TestOne(t *testing.T) {
+	Hogefunc1()
 	log.Println("TestOne running")
 }
 
 func TestTwo(t *testing.T) {
+	Hogefunc2()
 	log.Println("TestTwo running")
+	t.Fail()
 }
